9b173d5694890b429225dcc95aa2509a5ca8e5dd
Chih-Yuan Chang <cc908038@Broadcom.net>
2019-10-22 15:32:34 -0700

    TEMP

---
+++ test.cc
@@ -0,0 +1,21 @@
+#include <stdio>
+
+int main(argc, argv) {
+    for (auto &&i : v)
+    {
+        
+    }
+    
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    for (int i = length - 1; i >= 0; i--)
+    {
+        /* code */
+    }
+    
+
+
+}
---
+++ test2.cc
@@ -0,0 +1,40 @@
+#include <stdio>
+#include "function.cc"
+
+class myClass
+{
+private:
+    int y;
+public:
+    myClass(int x);
+    ~myClass();
+};
+
+myClass::myClass(int x)
+{
+    y = f(x);
+}
+
+myClass::~myClass()
+{
+}
+
+
+
+int main(argc, argv)
+{
+    myClass c();
+
+    for (int i = length - 1; i >= 0; i--)
+    {
+        /* code */
+        fopen("hello.dat")
+    }
+
+    for (size_t i = 0; i < count; i++)
+    {
+        int y = f(i);
+    }
+
+    int z = f(100);
+}
